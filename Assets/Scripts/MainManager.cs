d6d5d2e8f6fd2c8e63b19a7133c667826214ce45
Dennis Hackethal <dh@dennishackethal.com>
2024-01-01 12:25:48 -0700

    Housekeeping

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -15,13 +15,20 @@ public class MainManager : MonoBehaviour
         // This has to be a singleton or else the main manager
         // is instantiated again when the user returns from the
         // main scene to the menu scene.
+        // But why implement all the singleton stuff when you
+        // could instead have a static class that isn't attached
+        // to any game object? Then you don't need to worry about
+        // destroying duplicate instances (there are NO instances),
+        // and you can still store your data and access it from
+        // anywhere. You won't even need DonDestroyOnLoad.
+        // I'm a Unity noob but this strikes me as code smell.
+        // OOP isn't all that.
         if (Instance != null)
         {
             Destroy(gameObject);
             return;
         }
 
-        Debug.Log("awake");
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
