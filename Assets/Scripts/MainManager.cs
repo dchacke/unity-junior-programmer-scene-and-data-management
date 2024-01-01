55a972391d7884a3182468a57e77175a4b1b821a
Dennis Hackethal <dh@dennishackethal.com>
2024-01-01 14:45:46 -0700

    Add a LoadColor method

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -49,4 +49,17 @@ public class MainManager : MonoBehaviour
 
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
 }
